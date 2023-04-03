7a772b52cd556a5f3d725de6ff72c02f4172d343
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-04-03 16:51:36 -0700

    Add POST route for /login

--- express_server.js
+++ express_server.js
@@ -39,6 +39,12 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${id}`);
 });
 
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect('urls');
+});
+
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
